137bcafa3d3ca45be904803b248f4cb56c8e5ee6
Jazmin Matos <jmatos1993@gmail.com>
2020-08-26 23:44:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
     n = nil
     k = nil
   name_hash.each do |key, value| 
-    if value 
+    if value nil
       
     else value < n
      n = value #reassign value of n 
