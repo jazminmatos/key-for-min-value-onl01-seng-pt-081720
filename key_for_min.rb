da5b257b3ca4c00ec7367d1b332adec75e34a1f5
Jazmin Matos <jmatos1993@gmail.com>
2020-08-26 23:39:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-      n = 1/0.0
+    n = 501
   name_hash.each do |key, value| 
     if value < n
     return n = value #reassign value of n 
